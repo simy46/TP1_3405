6b92fe86ab76abb42a75071bd4abf37a19af9070
Missipsa Annane <missiannane@yahoo.com>
2024-01-16 17:26:27 -0500

    'allo'

--- Tp1_3405/src/ClientHandler.java
+++ Tp1_3405/src/ClientHandler.java
@@ -10,7 +10,7 @@ public class ClientHandler extends Thread { // pour traiter la demande de chaque
 		this.socket = socket;
 		this.clientNumber = clientNumber; 
 		System.out.println("New connection with client#" + clientNumber + " at" + socket);}
-	public void run() { // Création de thread qui envoi un message à un client
+	public void run() {// Création de thread qui envoi un message à un client
 		try {
 			DataOutputStream out = new DataOutputStream(socket.getOutputStream()); // création de canal d’envoi 
 			out.writeUTF("Hello from server - you are client#" + clientNumber); // envoi de message
@@ -23,31 +23,4 @@ public class ClientHandler extends Thread { // pour traiter la demande de chaque
 				System.out.println("Couldn't close a socket, what's going on?");}
 			System.out.println("Connection with client# " + clientNumber+ " closed");}
 	}
-=======
-
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.Socket;
-public class ClientHandler extends Thread { // pour traiter la demande de chaque client sur un socket particulier
-	private Socket socket; 
-	private int clientNumber; 
-	
-	public ClientHandler(Socket socket, int clientNumber) {
-		this.socket = socket;
-		this.clientNumber = clientNumber; 
-		System.out.println("New connection with client#" + clientNumber + " at" + socket);}
-	public void run() { // Création de thread qui envoi un message à un client
-		try {
-			DataOutputStream out = new DataOutputStream(socket.getOutputStream()); // création de canal d’envoi 
-			out.writeUTF("Hello from server - you are client#" + clientNumber); // envoi de message
-			} catch (IOException e) {
-			System.out.println("Error handling client# " + clientNumber + ": " + e);
-		} finally {
-			try {
-				socket.close();
-			} catch (IOException e) {
-				System.out.println("Couldn't close a socket, what's going on?");}
-			System.out.println("Connection with client# " + clientNumber+ " closed");}
-	}
->>>>>>> dadde49a6bdeab7d469d8400d17ac36576f5fdba
 }
