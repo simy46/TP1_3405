facdc5920c5342701fde311fa0c6b2865022461f
ROADROLLERD4 <89952389+ROADROLLERD4@users.noreply.github.com>
2024-01-17 15:40:59 -0500

    Ajout de la fonction pour la verification de l'adresse IP. >MK

--- Tp1_3405/src/Client.java
+++ Tp1_3405/src/Client.java
@@ -4,55 +4,86 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-    private static Socket socket;
-    private static String username;
-    private static String password;
+	private static Socket socket;
+	private static String username;
+	private static String password;
 
-    public boolean verificationPort(int port) {
-        return port > 5000 && port < 5050;
-    }
+	public boolean verificationPort(int port) {
+		return port > 5000 && port < 5050;
+	}
 
-    public void verificationipAdresse(char[] ipAddress) {
-    	
-    }
-    
+	public static boolean ipVerification(String ipAdress){
+		// Vérifier s'il y a des points consécutifs ou des points au début ou à la fin
+		if (ipAdress.startsWith(".") || ipAdress.endsWith(".") || ipAdress.contains("..")) return false;
+		//traitement des cas ou la string est vide ou quelle a plus de caractere qu e permis.
+		if (ipAdress.isEmpty() || ipAdress.length() > 15) return false;
 
-    public static void main(String[] args) {
-        try {
-            // Adresse et port du serveur
-            String serverAddress = "10.200.12.99";
-            int port = 5030;
+		//split du string que l'utilisateur à input
+		String[] split = ipAdress.split("\\.");
 
-            Scanner usernameScanner = new Scanner(System.in);
+		//verification que la structure de IpV4 est respectee. xxx.xxx.xxx.xxx (4 points max)
+		if (split.length != 4) return false;
+
+		//Cast les les string du tableau en int pour verifier qu'il n'y a pas de lettres
+		for ( String i: split) {
+			int number;
+			try {
+				number = Integer.parseInt(i);
+			} catch (NumberFormatException error) {
+				return false;
+			}
+			if (number > 255 || number < 0) return false;
+		}
+		return true; // si tous les etpaes de verifications sont passees.
+	}
+
+
+	public static void main(String[] args) {
+		try { 
+			// Adresse et port du serveur
+			String serverAddress; //"10.200.12.99"
+			int port = 5030;
+
+			boolean correctFormat;
+			do {
+				Scanner scanner = new Scanner(System.in);
+				System.out.println("Entrez l'adresse ipv4 du serveur :");
+				serverAddress = scanner.nextLine();
+				correctFormat = ipVerification(serverAddress);
+				if (!correctFormat) System.out.println("mauvais format d'adresse ip, veuillez recommencer. \n");
+
+			} while (!correctFormat);
+
+			Scanner usernameScanner = new Scanner(System.in);
 			System.out.println("Enter username");
 			username = usernameScanner.nextLine();
-			
-            Scanner passwordScanner = new Scanner(System.in);
+
+			Scanner passwordScanner = new Scanner(System.in);
 			System.out.println("Enter password");
 			password = passwordScanner.nextLine();
-			
-            
-            // Création d'une nouvelle connexion avec le serveur
-            socket = new Socket(serverAddress, port);
-            System.out.format("Connecté au serveur sur [%s : %d]%n", serverAddress, port);
 
-            // Création des canaux de communication avec le serveur
-            DataOutputStream outClient = new DataOutputStream(socket.getOutputStream());
-            DataInputStream inClient = new DataInputStream(socket.getInputStream());
 
-            // Envoi du nom d'utilisateur et du mot de passe au serveur
-            outClient.writeUTF(username);
-            outClient.writeUTF(password);
+			// Création d'une nouvelle connexion avec le serveur
+			socket = new Socket(serverAddress, port);
+			System.out.format("Connecté au serveur sur [%s : %d]%n", serverAddress, port);
 
-            // Attente de la réception d'un message envoyé par le serveur sur le canal
-            String helloMessageFromServer = inClient.readUTF();
-            System.out.println(helloMessageFromServer);
+			// Création des canaux de communication avec le serveur
+			DataOutputStream outClient = new DataOutputStream(socket.getOutputStream());
+			DataInputStream inClient = new DataInputStream(socket.getInputStream());
 
-            // Fermeture de la connexion avec le serveur
-            socket.close();
+			// Envoi du nom d'utilisateur et du mot de passe au serveur
+			outClient.writeUTF(username);
+			outClient.writeUTF(password);
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+			// Attente de la réception d'un message envoyé par le serveur sur le canal
+			String helloMessageFromServer = inClient.readUTF();
+			System.out.println(helloMessageFromServer);
+
+			// Fermeture de la connexion avec le serveur
+			socket.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
