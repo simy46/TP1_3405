db2732822bb8ca9cb207b1d0d65fd1151e15ce0d
Missipsa Annane <missiannane@yahoo.com>
2024-01-22 18:30:27 -0500

    'allo'

--- Tp1_3405/src/Serveur.java
+++ Tp1_3405/src/Serveur.java
@@ -1,4 +1,5 @@
 
+import java.util.*;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
@@ -9,6 +10,49 @@ import java.io.DataInputStream;
 public class Serveur {
 
 	private static ServerSocket Listener;
+	private static Map<String, String> DataBase;
+	
+	public static Map<String, String> getDataBase() {
+		return DataBase;
+	}
+
+	public static void setDataBase(Map<String, String> dataBase) {
+		DataBase = dataBase;
+	}
+	
+    public static boolean usernameExist(String username) {
+        return DataBase.get(username) != null;
+    }
+	
+
+	public static boolean verificationPort(int port) {
+		return port > 5000 && port < 5050;
+	}
+
+	public static boolean ipVerification(String ipAdress){
+		// Vérifier s'il y a des points consécutifs ou des points au début ou à la fin
+		if (ipAdress.startsWith(".") || ipAdress.endsWith(".") || ipAdress.contains("..")) return false;
+		//traitement des cas ou la string est vide ou quelle a plus de caractere qu e permis.
+		if (ipAdress.isEmpty() || ipAdress.length() > 15) return false;
+
+		//split du string que l'utilisateur à input
+		String[] split = ipAdress.split("\\.");
+
+		//verification que la structure de IpV4 est respectee. xxx.xxx.xxx.xxx (4 points max)
+		if (split.length != 4) return false;
+
+		//Cast les les string du tableau en int pour verifier qu'il n'y a pas de lettres
+		for ( String i: split) {
+			int number;
+			try {
+				number = Integer.parseInt(i);
+			} catch (NumberFormatException error) {
+				return false;
+			}
+			if (number > 255 || number < 0) return false;
+		}
+		return true; // si tous les etpaes de verifications sont passees.
+	}
 	
 
 	// Application Serveur
@@ -41,6 +85,8 @@ public class Serveur {
 
 		        // Lecture du nom d'utilisateur et du mot de passe du client
 		        String userName = dataInputStream.readUTF();
+		        
+		        
 
 		        // Création d'une nouvelle instance de ClientHandler pour gérer le client
 		        new ClientHandler(clientSocket, clientNumber++, userName).start();
