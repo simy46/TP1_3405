a1db94f93b1feb039994482712ad52b23e278043
Missipsa Annane <missiannane@yahoo.com>
2024-01-24 09:37:06 -0500

    'allo'

--- Tp1_3405/src/Serveur.java
+++ Tp1_3405/src/Serveur.java
@@ -5,32 +5,73 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.DataInputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
 
 public class Serveur {
 
 	private static ServerSocket Listener;
 	private static Map<String, String> DataBase;
+	private static int port;
+	private static String serverAdress;
 	
 	public static Map<String, String> getDataBase() {
 		return DataBase;
-	}
-
-	public static void setDataBase(Map<String, String> dataBase) {
-		DataBase = dataBase;
 	}
 	
     public static boolean usernameExist(String username) {
         return DataBase.get(username) != null;
     }
-		
+    
+    // Permet de LIRE le fichier texte //
+    public static void loadUserDatabase(String filePath) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 2) {
+                    String username = parts[0].trim();
+                    String password = parts[1].trim();
+                    DataBase.put(username, password);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	
+    // Permet d'ÉCRIRE dans le fichier texte //
+    public static void writeToUserFile(String filePath, String username, String password) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
+            writer.write(username + "," + password);
+            writer.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    // si client EXISTE => Vérifier MDP // sinon on écrit ses infos //
+    private static boolean validateUserCredentials(String receivedUsername, String receivedPassword) {
+        if (DataBase.containsKey(receivedUsername)) {
+            return DataBase.get(receivedUsername).equals(receivedPassword);
+        } else {
+            DataBase.put(receivedUsername, receivedPassword);
+            writeToUserFile("dataBase.txt", receivedUsername, receivedPassword);
+            return true;
+        }
+    }
 
 	// Application Serveur
 	public static void main(String[] args) throws Exception {
 		
 		// Compteur incrémenté à chaque connexion d'un client au serveur
 		int clientNumber = 0;
-		// Client.askingForIpAndPort();
+		// Client.askingForIpAndPort(); À REVOIR CAR PRIVATE ATTRIBUT DE CLIENT DANS CETTE FONCTION //
 		
 		// Adresse et port du serveur
 		String serverAddress = "10.200.12.99";
